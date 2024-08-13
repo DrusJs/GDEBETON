9aa76ea42969277e7cdab5b2c95d75f71a659ae4
drusjs <magnojoky962@gmail.com>
2024-08-13 09:09:48 +0300

    fix add events for selects

--- js/main.js
+++ js/main.js
@@ -12,10 +12,31 @@ document.addEventListener("DOMContentLoaded", () => {
         })
     }
 
+
     const burgerButton = document.querySelector('.burger__button')
     const navMenu = document.querySelector('.nav-menu ')
 
     burgerButton.addEventListener('click', () => {
         navMenu.classList.toggle('active')
     })
+
+    
+    const selectButtons = document.querySelectorAll('.select-button')
+
+    selectButtons.forEach((select) => {
+        select.addEventListener('click', (e) => {
+            e.currentTarget.closest('.select-wrapper').classList.toggle('active')
+        })
+
+        const selectItems = select.closest('.select-wrapper').querySelectorAll('.select-dropdown__item')
+
+        selectItems.forEach((item) => {
+            item.addEventListener('click', (e) => {
+                select.querySelector('.select-value').innerHTML = e.currentTarget.innerHTML
+                e.currentTarget.closest('.select-wrapper').classList.toggle('active')
+            })
+        })
+    })
+
+    
 });
